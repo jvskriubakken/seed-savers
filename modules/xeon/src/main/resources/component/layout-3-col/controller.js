a669b16ce6e8f4036eac2c834e174c1f0442b0f3
Sten Roger Sandvik <srs@enonic.com>
2014-09-09 10:37:14 +0200

    Converted two controller.js scripts.

---
+++ modules/xeon/src/main/resources/component/layout-3-col/controller.js
@@ -0,0 +1,26 @@
+var thymeleaf = require('view/thymeleaf');
+
+exports.get = function (portal) {
+    var editMode = portal.request.mode == 'edit';
+
+    var content = portal.content;
+    var component = portal.component;
+    var layoutRegions = portal.layoutRegions;
+
+    var view = resolve('/view/layout-3-col.html');
+    var body = thymeleaf.render(view, {
+        title: content.displayName,
+        path: content.path,
+        name: content.name,
+        editable: editMode,
+        resourcesPath: portal.url.createResourceUrl(''),
+        component: component,
+        leftRegion: layoutRegions.getRegion("left"),
+        centerRegion: layoutRegions.getRegion("center"),
+        rightRegion: layoutRegions.getRegion("right")
+    });
+
+    portal.response.body = body;
+    portal.response.contentType = 'text/html';
+    portal.response.status = 200;
+};
---
+++ modules/xeon/src/main/resources/page/banner-page/controller.js
@@ -0,0 +1,49 @@
+var contentService = require('contentService');
+var thymeleaf = require('view/thymeleaf');
+
+function handleGet(portal) {
+    var content = portal.content;
+    var page = portal.content.page;
+    var pageRegions = portal.pageRegions;
+    var site = portal.siteContent;
+    var editMode = portal.request.mode == 'edit';
+    var slides = page ? page.config.dataSets("slide") : [];
+
+    var xeonConfig = portal.siteContent.site.moduleConfigs.get('com.enonic.wem.modules.xeon-1.0.0').getConfig();
+
+    var params = {
+        context: portal,
+        pageRegions: pageRegions,
+        mainRegion: pageRegions.getRegion("main"),
+        contents: contentService.getChildContent(site.path),
+        editable: editMode,
+        banner: true,
+        slides: slides,
+        site: site,
+        moduleConfig: xeonConfig,
+        content: content,
+        logoUrl: getLogoUrl(portal, xeonConfig)
+    };
+
+    var view = resolve('../../view/page.html');
+    var body = thymeleaf.render(view, params);
+
+    portal.response.contentType = 'text/html';
+    portal.response.body = body;
+}
+
+function getLogoUrl(portal, xeonConfig) {
+    var logoContent;
+    var logo = xeonConfig.getProperty('logo');
+    if (logo) {
+        logoContent = system.contentService.getContentById(logo.getString());
+    }
+
+    if (logoContent) {
+        return portal.url.createImageByIdUrl(logoContent.id).filter("scaleblock(115,26)");
+    } else {
+        return portal.url.createResourceUrl('images/logo.png');
+    }
+}
+
+exports.get = handleGet;
