0fb89b5c1878e415d4c2311e5f82388b36b9bb0b
Jørund Vier Skriubakken <jvs@enonic.com>
2014-10-21 12:11:59 +0200

    SeedSavers: Added example usage of Metadata system:menu in member controller

--- modules/seedsavers/src/main/resources/page/member/controller.js
+++ modules/seedsavers/src/main/resources/page/member/controller.js
@@ -1,16 +1,35 @@
 var thymeleaf = require('view/thymeleaf');
 
+
+
 function handleGet(portal) {
 
     var editMode = portal.request.mode == 'edit';
     var view = resolve('./member.page.html');
+
+
+    var defaultMenu = {
+        menu: [false],
+        menuName: [portal.content.displayName]
+    };
+
+    var menu;
+    if( portal.content.hasMetadata("system:menu") ) {
+        menu = portal.content.getMetadata("system:menu").toMap();
+    }
+    else {
+        menu = defaultMenu;
+    }
+
+
     var params = {
         context: portal,
         site: portal.site,
         content: portal.content,
         pageConfig: portal.content.page.config.toMap(),
         mainRegion: portal.content.page.getRegion("main"),
-        editable: editMode
+        editable: editMode,
+        menu: menu
     };
     var body = thymeleaf.render(view, params);
 
