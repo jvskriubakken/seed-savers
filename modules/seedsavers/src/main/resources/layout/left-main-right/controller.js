65a88e4c64d9053bee169c77f2173bd9e6fb2b49
Jørund Vier Skriubakken <jvs@enonic.com>
2015-01-13 13:41:27 +0100

    SeedSavers: Fixed retrieving of requested component

--- modules/seedsavers/src/main/resources/layout/left-main-right/controller.js
+++ modules/seedsavers/src/main/resources/layout/left-main-right/controller.js
@@ -3,15 +3,15 @@ var thymeleaf = require('/lib/view/thymeleaf');
 function handleGet(req) {
 
     var editMode = req.mode == 'edit';
-    var component = req.component;
+    var component = execute('portal.getComponent');
     var view = resolve('./left-main-right.html');
 
     var params = {
         editable: editMode,
         component: component,
-        leftRegion: component.getRegion("left"),
-        mainRegion: component.getRegion("main"),
-        rightRegion: component.getRegion("right")
+        leftRegion: component.regions["left"],
+        mainRegion: component.regions["main"],
+        rightRegion: component.regions["right"]
     };
 
     var body = thymeleaf.render(view, params);
